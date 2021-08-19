b8c3b4957a8804d11f7b8ed174f3939e7bdd2dfb
Victor Petukhov <victor.petukhov@jetbrains.com>
2021-08-19 09:08:39 +0300

    Resolve calls using the builder inference despite the presence of the annotation if there are uninferred type variables
    ^KT-48194 Fixed

--- compiler/tests-gen/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
+++ compiler/tests-gen/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
@@ -14974,6 +14974,11 @@ public class LightAnalysisModeTestGenerated extends AbstractLightAnalysisModeTes
             public void testWithExpectedType() throws Exception {
                 runTest("compiler/testData/codegen/box/inference/builderInference/withExpectedType.kt");
             }
+
+            @TestMetadata("withoutAnnotation.kt")
+            public void testWithoutAnnotation() throws Exception {
+                runTest("compiler/testData/codegen/box/inference/builderInference/withoutAnnotation.kt");
+            }
         }
     }
 
