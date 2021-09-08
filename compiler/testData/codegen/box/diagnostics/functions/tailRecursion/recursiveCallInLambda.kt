1d504418cf0a7e4ee4c5473e4028080a28fc1bd7
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2021-09-08 12:15:18 +0300

    [TESTS] Remove ignore js backend directive from some tailRecursion tests
    These tests were falling due to inserted diagnostics. New test
    infrastructure can remove them before running test.

--- compiler/testData/codegen/box/diagnostics/functions/tailRecursion/recursiveCallInLambda.kt
+++ compiler/testData/codegen/box/diagnostics/functions/tailRecursion/recursiveCallInLambda.kt
@@ -3,10 +3,6 @@
 // !DIAGNOSTICS: -UNUSED_PARAMETER
 // IGNORE_BACKEND: JS_IR
 // IGNORE_BACKEND: JS_IR_ES6
-
-// TODO: muted automatically, investigate should it be ran for JS or not
-// DONT_RUN_GENERATED_CODE: JS
-// IGNORE_BACKEND: JS
 // IGNORE_FIR_DIAGNOSTICS_DIFF
 
 <!NO_TAIL_CALLS_FOUND!>tailrec fun foo()<!> {
