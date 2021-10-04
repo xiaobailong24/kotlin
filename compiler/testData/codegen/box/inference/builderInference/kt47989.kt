81746af1244e9702869cb8aa0928395483400f8c
Victor Petukhov <victor.petukhov@jetbrains.com>
2021-10-04 12:00:14 +0300

    Propagate inference session from local context into property resolvers
    ^KT-47989 Fixed

---
+++ compiler/testData/codegen/box/inference/builderInference/kt47989.kt
@@ -0,0 +1,17 @@
+// WITH_RUNTIME
+
+class TowerDataElementsForName() {
+    @OptIn(ExperimentalStdlibApi::class)
+    val reversedFilteredLocalScopes = buildList {
+        class Foo {
+            val reversedFilteredLocalScopes = {
+                add("OK")
+            }
+        }
+        Foo().reversedFilteredLocalScopes()
+    }
+}
+
+fun box(): String {
+    return TowerDataElementsForName().reversedFilteredLocalScopes[0]
+}
