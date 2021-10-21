b172c4e83ed2b108d0fd0cc5e0d7b2a543772215
mcpiroman <mcpiroman@gmail.com>
2021-10-21 19:55:56 +0200

    Switch to auto generated IR tree
    Co-authored-by: Alexander Udalov <alexander.udalov@jetbrains.com>

---
+++ compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/declarations/IrOverridableMember.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+//This file was generated automatically
+//DO NOT MODIFY IT MANUALLY
+
+package org.jetbrains.kotlin.ir.declarations
+
+import org.jetbrains.kotlin.descriptors.Modality
+
+/**
+ * A non-leaf IR tree element.
+ * @sample org.jetbrains.kotlin.ir.generator.IrTree.overridableMember
+ */
+interface IrOverridableMember : IrDeclaration, IrDeclarationWithVisibility,
+        IrDeclarationWithName, IrSymbolOwner {
+    val modality: Modality
+}
