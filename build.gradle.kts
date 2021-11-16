38247db0e415185d66f1342462bda1b817c3d39e
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2021-11-16 23:23:13 +0100

    Suppress warnings in KGP additional compilations.
    These warnings were actually moved from main compiation.
    
    ^KT-49227 In Progress
    
    Signed-off-by: Yahor Berdnikau <yahor.berdnikau@jetbrains.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -323,7 +323,9 @@ extra["compilerArtifactsForIde"] = listOf(
 
 // TODO: fix remaining warnings and remove this property.
 extra["tasksWithWarnings"] = listOf(
-    ":kotlin-gradle-plugin:compileKotlin"
+    ":kotlin-gradle-plugin:compileKotlin",
+    ":kotlin-gradle-plugin:compileCommonKotlin",
+    ":kotlin-gradle-plugin:compileGradle70Kotlin"
 )
 
 val tasksWithWarnings: List<String> by extra
