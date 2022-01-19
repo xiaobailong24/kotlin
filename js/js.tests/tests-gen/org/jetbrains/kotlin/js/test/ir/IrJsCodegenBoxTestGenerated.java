d11d2d2168b6114dd0d16dd138aa9e7feb8b5dba
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2022-01-19 02:39:44 +0100

    Check for sealed inline classes in turning abstract methods to open
    and in bridge lowering.
    
     #KT-27576

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/IrJsCodegenBoxTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/IrJsCodegenBoxTestGenerated.java
@@ -19585,6 +19585,12 @@ public class IrJsCodegenBoxTestGenerated extends AbstractIrJsCodegenBoxTest {
             }
 
             @Test
+            @TestMetadata("is.kt")
+            public void testIs() throws Exception {
+                runTest("compiler/testData/codegen/box/inlineClasses/sealed/is.kt");
+            }
+
+            @Test
             @TestMetadata("object.kt")
             public void testObject() throws Exception {
                 runTest("compiler/testData/codegen/box/inlineClasses/sealed/object.kt");
