1ccbba752b3d01e0eba38d9a45284d1f7515cbf7
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2022-01-20 17:17:17 +0100

    [WASM] throwableExtensions implementation

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/compiler.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/compiler.kt
@@ -120,7 +120,11 @@ fun WasmCompiledModuleFragment.generateJs(): String {
 
         println(valueAddr) {
             console.log(">>>  " + importStringFromWasm(valueAddr));
-        }
+        },
+        
+        printError(valueAddr) {
+            console.error(">>>  " + importStringFromWasm(valueAddr));
+        },
     };
     
     function importStringFromWasm(addr) {
