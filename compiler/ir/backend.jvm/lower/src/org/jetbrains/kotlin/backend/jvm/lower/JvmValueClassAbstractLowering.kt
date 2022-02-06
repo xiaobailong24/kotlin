5ae8796f4c3131b691a068ee554114e8d83e8c08
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2022-02-06 13:23:00 +0100

    Add constructor-impl for sealed inline class children
     #KT-27576

--- compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/JvmValueClassAbstractLowering.kt
+++ compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/JvmValueClassAbstractLowering.kt
@@ -11,6 +11,7 @@ import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
 import org.jetbrains.kotlin.backend.jvm.InlineClassAbi
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
 import org.jetbrains.kotlin.backend.jvm.MemoizedValueClassAbstractReplacements
+import org.jetbrains.kotlin.backend.jvm.ir.isInlineClassType
 import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.builders.irReturn
@@ -65,7 +66,7 @@ internal abstract class JvmValueClassAbstractLowering(val context: JvmBackendCon
                 (it == irConstructor && declaration.modality != Modality.SEALED) ||
                         (it is IrFunction && it.isSpecificFieldGetter() && !it.visibility.isPublicAPI)
             }
-            if (declaration.modality != Modality.SEALED && !declaration.isChildOfSealedInlineClass()) {
+            if (declaration.modality != Modality.SEALED && !declaration.superTypes.any { it.isInlineClassType() }) {
                 buildPrimaryValueClassConstructor(declaration, irConstructor)
                 buildBoxFunction(declaration)
                 buildUnboxFunctions(declaration)
