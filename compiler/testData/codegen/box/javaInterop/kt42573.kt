a7f83c42df8352cbf8749ecaac93ae5016eede64
Victor Petukhov <victor.petukhov@jetbrains.com>
2022-02-21 13:50:44 +0300

    [FE] Add test for KT-42573

---
+++ compiler/testData/codegen/box/javaInterop/kt42573.kt
@@ -0,0 +1,14 @@
+// TARGET_BACKEND: JVM
+
+// FILE: TestJava.java
+public class TestJava {
+    public static String op(Object object) { return "NOK"; }
+    public static String op(Object[] objects) { return "OK"; }
+}
+
+// FILE: main.kt
+fun box(): String {
+    var i: Int? = 1
+    i = null
+    return TestJava.op(i)
+}
