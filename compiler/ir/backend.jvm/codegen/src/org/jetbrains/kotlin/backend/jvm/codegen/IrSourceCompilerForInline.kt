0d3446363e135ab15e4e56c3cae71340102fa672
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2022-04-04 06:28:51 +0200

    Coerce to underlying type in inline lambda after unboxing
     #KT-27576

--- compiler/ir/backend.jvm/codegen/src/org/jetbrains/kotlin/backend/jvm/codegen/IrSourceCompilerForInline.kt
+++ compiler/ir/backend.jvm/codegen/src/org/jetbrains/kotlin/backend/jvm/codegen/IrSourceCompilerForInline.kt
@@ -11,6 +11,7 @@ import org.jetbrains.kotlin.backend.common.ir.allParameters
 import org.jetbrains.kotlin.backend.common.ir.ir2string
 import org.jetbrains.kotlin.backend.jvm.hasMangledReturnType
 import org.jetbrains.kotlin.backend.jvm.ir.*
+import org.jetbrains.kotlin.backend.jvm.mapping.mapType
 import org.jetbrains.kotlin.codegen.StackValue
 import org.jetbrains.kotlin.codegen.inline.*
 import org.jetbrains.kotlin.codegen.state.GenerationState
@@ -24,6 +25,8 @@ import org.jetbrains.kotlin.ir.declarations.name
 import org.jetbrains.kotlin.ir.descriptors.toIrBasedDescriptor
 import org.jetbrains.kotlin.ir.expressions.IrFunctionAccessExpression
 import org.jetbrains.kotlin.ir.expressions.IrLoop
+import org.jetbrains.kotlin.ir.types.classOrNull
+import org.jetbrains.kotlin.ir.types.makeNotNull
 import org.jetbrains.kotlin.ir.types.makeNullable
 import org.jetbrains.kotlin.ir.util.*
 import org.jetbrains.kotlin.psi.doNotAnalyze
@@ -141,6 +144,8 @@ class IrSourceCompilerForInline(
         val topIrType = irType.findTopSealedInlineSuperClass().defaultType
         val topType = codegen.typeMapper.mapType(topIrType.makeNullable())
         StackValue.unboxInlineClass(AsmTypes.OBJECT_TYPE, topType, AsmTypes.OBJECT_TYPE, false, mv)
+        val jvmType = codegen.typeMapper.mapType(irType.makeNotNull())
+        StackValue.coerce(AsmTypes.OBJECT_TYPE, jvmType, mv)
     }
 }
 
