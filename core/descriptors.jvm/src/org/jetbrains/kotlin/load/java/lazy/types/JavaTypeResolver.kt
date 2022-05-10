1c1f2a61c97a5f438a4b7fadaa4c568f032bc1dd
Victor Petukhov <victor.petukhov@jetbrains.com>
2022-05-10 09:22:25 +0200

    Revert "[FE 1.0] Unbind type parameter erasing from java"
    This reverts commit f31cf90de2763577500835de8e35fbe5376805f2.

--- core/descriptors.jvm/src/org/jetbrains/kotlin/load/java/lazy/types/JavaTypeResolver.kt
+++ core/descriptors.jvm/src/org/jetbrains/kotlin/load/java/lazy/types/JavaTypeResolver.kt
@@ -20,9 +20,9 @@ import org.jetbrains.kotlin.builtins.jvm.JavaToKotlinClassMapper
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.descriptors.TypeParameterDescriptor
 import org.jetbrains.kotlin.descriptors.annotations.Annotations
-import org.jetbrains.kotlin.types.TypeUsage
-import org.jetbrains.kotlin.types.TypeUsage.COMMON
-import org.jetbrains.kotlin.types.TypeUsage.SUPERTYPE
+import org.jetbrains.kotlin.load.java.components.TypeUsage
+import org.jetbrains.kotlin.load.java.components.TypeUsage.COMMON
+import org.jetbrains.kotlin.load.java.components.TypeUsage.SUPERTYPE
 import org.jetbrains.kotlin.load.java.lazy.LazyJavaAnnotations
 import org.jetbrains.kotlin.load.java.lazy.LazyJavaResolverContext
 import org.jetbrains.kotlin.load.java.lazy.TypeParameterResolver
@@ -31,7 +31,6 @@ import org.jetbrains.kotlin.load.java.structure.*
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.types.*
-import org.jetbrains.kotlin.types.TypeUtils.makeStarProjection
 import org.jetbrains.kotlin.types.Variance.*
 import org.jetbrains.kotlin.types.error.ErrorUtils
 import org.jetbrains.kotlin.types.error.ErrorTypeKind
@@ -44,8 +43,8 @@ class JavaTypeResolver(
     private val c: LazyJavaResolverContext,
     private val typeParameterResolver: TypeParameterResolver
 ) {
-    private val projectionComputer = RawProjectionComputer()
-    private val typeParameterUpperBoundEraser = TypeParameterUpperBoundEraser(projectionComputer)
+    private val typeParameterUpperBoundEraser = TypeParameterUpperBoundEraser()
+    private val rawSubstitution = RawSubstitution(typeParameterUpperBoundEraser)
 
     fun transformJavaType(javaType: JavaType?, attr: JavaTypeAttributes): KotlinType {
         return when (javaType) {
@@ -226,15 +225,15 @@ class JavaTypeResolver(
         val erasedUpperBound = LazyWrappedType(c.storageManager) {
             typeParameterUpperBoundEraser.getErasedUpperBound(
                 parameter,
-                attr.withDefaultType(constructor.declarationDescriptor?.defaultType).markIsRaw(javaType.isRaw)
+                javaType.isRaw,
+                attr.withDefaultType(constructor.declarationDescriptor?.defaultType)
             )
         }
 
-        projectionComputer.computeProjection(
+        rawSubstitution.computeProjection(
             parameter,
             // if erasure happens due to invalid arguments number, use star projections instead
-            attr.markIsRaw(javaType.isRaw),
-            typeParameterUpperBoundEraser,
+            if (javaType.isRaw) attr else attr.withFlexibility(INFLEXIBLE),
             erasedUpperBound
         )
     }
@@ -310,3 +309,42 @@ class JavaTypeResolver(
         return !isForAnnotationParameter && howThisTypeIsUsed != SUPERTYPE
     }
 }
+
+internal fun makeStarProjection(
+    typeParameter: TypeParameterDescriptor,
+    attr: JavaTypeAttributes
+): TypeProjection {
+    return if (attr.howThisTypeIsUsed == SUPERTYPE)
+        TypeProjectionImpl(typeParameter.starProjectionType())
+    else
+        StarProjectionImpl(typeParameter)
+}
+
+data class JavaTypeAttributes(
+    val howThisTypeIsUsed: TypeUsage,
+    val flexibility: JavaTypeFlexibility = INFLEXIBLE,
+    val isForAnnotationParameter: Boolean = false,
+    // we use it to prevent happening a recursion while compute type parameter's upper bounds
+    val visitedTypeParameters: Set<TypeParameterDescriptor>? = null,
+    val defaultType: SimpleType? = null
+) {
+    fun withFlexibility(flexibility: JavaTypeFlexibility) = copy(flexibility = flexibility)
+    fun withDefaultType(type: SimpleType?) = copy(defaultType = type)
+    fun withNewVisitedTypeParameter(typeParameter: TypeParameterDescriptor) =
+        copy(visitedTypeParameters = if (visitedTypeParameters != null) visitedTypeParameters + typeParameter else setOf(typeParameter))
+}
+
+enum class JavaTypeFlexibility {
+    INFLEXIBLE,
+    FLEXIBLE_UPPER_BOUND,
+    FLEXIBLE_LOWER_BOUND
+}
+
+fun TypeUsage.toAttributes(
+    isForAnnotationParameter: Boolean = false,
+    upperBoundForTypeParameter: TypeParameterDescriptor? = null
+) = JavaTypeAttributes(
+    this,
+    isForAnnotationParameter = isForAnnotationParameter,
+    visitedTypeParameters = upperBoundForTypeParameter?.let(::setOf)
+)
--- core/descriptors/src/org/jetbrains/kotlin/types/TypeUtils.java
+++ core/descriptors/src/org/jetbrains/kotlin/types/TypeUtils.java
@@ -30,8 +30,6 @@ import org.jetbrains.kotlin.utils.SmartSet;
 
 import java.util.*;
 
-import static org.jetbrains.kotlin.types.TypeUsage.SUPERTYPE;
-
 public class TypeUtils {
     public static final SimpleType DONT_CARE = ErrorUtils.createErrorType(ErrorTypeKind.DONT_CARE);
     public static final SimpleType CANNOT_INFER_FUNCTION_PARAM_TYPE =
@@ -480,15 +478,6 @@ public class TypeUtils {
     @NotNull
     public static TypeProjection makeStarProjection(@NotNull TypeParameterDescriptor parameterDescriptor) {
         return new StarProjectionImpl(parameterDescriptor);
-    }
-
-    @NotNull
-    public static TypeProjection makeStarProjection(@NotNull TypeParameterDescriptor parameterDescriptor, ErasureTypeAttributes attr) {
-        if (attr.getHowThisTypeIsUsed() == SUPERTYPE) {
-            return new TypeProjectionImpl(StarProjectionImplKt.starProjectionType(parameterDescriptor));
-        } else {
-            return new StarProjectionImpl(parameterDescriptor);
-        }
     }
 
     @NotNull
