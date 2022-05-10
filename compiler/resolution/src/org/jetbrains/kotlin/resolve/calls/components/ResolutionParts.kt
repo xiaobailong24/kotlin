8ba8b28dac3273eb23a2ae6bcfc54e166b4948df
Victor Petukhov <victor.petukhov@jetbrains.com>
2022-05-10 09:22:03 +0200

    Revert "[FE 1.0] Report errors due to inferred empty intersection on those candidates which were already previously discriminated by `CompatibilityOfTypeVariableAsIntersectionTypePart`"
    This reverts commit 12a39d03

--- compiler/resolution/src/org/jetbrains/kotlin/resolve/calls/components/ResolutionParts.kt
+++ compiler/resolution/src/org/jetbrains/kotlin/resolve/calls/components/ResolutionParts.kt
@@ -29,7 +29,6 @@ import org.jetbrains.kotlin.types.error.ErrorUtils
 import org.jetbrains.kotlin.types.model.*
 import org.jetbrains.kotlin.types.typeUtil.*
 import org.jetbrains.kotlin.utils.SmartList
-import org.jetbrains.kotlin.utils.addToStdlib.cast
 import org.jetbrains.kotlin.utils.addToStdlib.compactIfPossible
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
@@ -881,13 +880,6 @@ internal object CheckContextReceiversResolutionPart : ResolutionPart() {
 }
 
 internal object CheckIncompatibleTypeVariableUpperBounds : ResolutionPart() {
-    /*
-     * Check if the candidate was already discriminated by `CompatibilityOfTypeVariableAsIntersectionTypePart` resolution part
-     * If it's true we shouldn't mark the candidate with warning, but should mark with error, to repeat the existing proper behaviour
-     */
-    private fun ResolutionCandidate.wasPreviouslyDiscriminated(upperTypes: List<KotlinTypeMarker>) =
-        callComponents.statelessCallbacks.isOldIntersectionIsEmpty(upperTypes.cast())
-
     override fun ResolutionCandidate.process(workIndex: Int) = with(getSystem().asConstraintSystemCompleterContext()) {
         for (variableWithConstraints in getSystem().getBuilder().currentStorage().notFixedTypeVariables.values) {
             val upperTypes = variableWithConstraints.constraints.extractUpperTypesToCheckIntersectionEmptiness()
@@ -896,9 +888,8 @@ internal object CheckIncompatibleTypeVariableUpperBounds : ResolutionPart() {
                 val isInferredEmptyIntersectionForbidden =
                     callComponents.languageVersionSettings.supportsFeature(LanguageFeature.ForbidInferringTypeVariablesIntoEmptyIntersection)
 
-                val errorFactory = if (isInferredEmptyIntersectionForbidden || wasPreviouslyDiscriminated(upperTypes))
-                    ::InferredEmptyIntersectionError
-                else ::InferredEmptyIntersectionWarning
+                val errorFactory =
+                    if (isInferredEmptyIntersectionForbidden) ::InferredEmptyIntersectionError else ::InferredEmptyIntersectionWarning
                 addError(errorFactory(upperTypes, variableWithConstraints.typeVariable))
             }
         }
